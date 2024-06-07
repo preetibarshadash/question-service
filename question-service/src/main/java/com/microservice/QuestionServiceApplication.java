d45d552699e96cec8c8e5980cdfac95043c68eac
preetibarshadash <preetivarshadash@gmail.com>
2024-06-07 23:46:45 +0530

    minor config changes

--- question-service/src/main/java/com/microservice/QuestionServiceApplication.java
+++ question-service/src/main/java/com/microservice/QuestionServiceApplication.java
@@ -4,10 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EntityScan("com.microservice.entity")
+@EnableFeignClients
 public class QuestionServiceApplication {
 
 	public static void main(String[] args) {
